71309ef9d03ea46aeaeb28549631cc6e097a46b5
Vyacheslav Napadovsky <napadovskiy@gmail.com>
2016-06-24 22:48:14 +0300

    old ignored binaries for ffmpeg build added

---
+++ build.user.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+SET "MPCHC_MINGW32=C:\MinGW"
+SET "MPCHC_MINGW64=%MPCHC_MINGW32%"
+SET "MPCHC_MSYS=%MPCHC_MINGW32%\msys\1.0"
+REM Git is optional to set if you chose to add git in PATH when installing it
+SET "MPCHC_GIT=C:\Program Files (x86)\Git"
---
+++ include/version_rev.h
@@ -0,0 +1,3 @@
+#define MPCHC_HASH _T("7d1831b")
+#define MPC_VERSION_REV 7378
+#define MPC_VERSION_REV_FULL _T("7378 (7d1831b)")
